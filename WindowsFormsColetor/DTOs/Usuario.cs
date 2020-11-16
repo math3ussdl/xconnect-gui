5475853e9ef5ca4d4b6e9490577c8b2415db419f
Juliherms de Lima Vasconcelos <jlv@REDENCAO-015591.cesar-ldap.intranet>
2020-11-16 07:28:55 -0300

    implementacao do cadastro de usuario

--- WindowsFormsColetor/DTOs/Usuario.cs
+++ WindowsFormsColetor/DTOs/Usuario.cs
@@ -6,8 +6,22 @@ namespace WindowsFormsColetor.DTOs
 {
     public class Usuario
     {
-        public int Codigo { get; set; } 
-        public String Login { get; set; }
-        public String Senha { get; set; }
+        public String Id { get; set; } 
+        public String Name { get; set; }
+        public String LastName { get; set; }
+        public String Cpf { get; set; }
+        public String Cnpj { get; set; }
+        public String Address { get; set; }
+        public String Email { get; set; }
+        public String Password { get; set; }
+        public String Phone { get; set; }
+        public String Zip { get; set; }
+        public String Role { get; set; }
+
+
+
+
+
+
     }
 }
